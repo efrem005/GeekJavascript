163a17094f354026aa52bb346b7114d636004522
Nikolay <efrem_nn@mail.ru>
2020-12-25 23:34:26 +0700

    Домашняя работа урока №2

---
+++ lesson2/humburger.js
@@ -0,0 +1,108 @@
+const burgerHtml = () => `
+<div class="row">
+<div class="col-lg-3 mb-4 col-md-4 col-sm-6">
+  <figure class="figure">
+    <img
+      src="https://s82079.cdn.ngenix.net/wK4fLvNXD4NKZJUrVgjPV383.png?dw=230"
+      class="figure-img img-fluid rounded"
+      alt="..."
+      id="big"
+      name="hamburger"
+    />
+    <figcaption class="figure-caption text-center">
+      Большой гамбургер
+    </figcaption>
+  </figure>
+</div>
+<div class="col-lg-3 mb-4 col-md-4 col-sm-6">
+  <figure class="figure">
+    <img
+      src="https://s82079.cdn.ngenix.net/X4eyRrugynKehEm2Wr8YEZeo.png?dw=230"
+      class="figure-img img-fluid rounded"
+      alt="..."
+      id="small"
+      name="hamburger"
+    />
+    <figcaption class="figure-caption text-center">
+      Маленький гамбургер
+    </figcaption>
+  </figure>
+</div>
+</div>
+<div class="row">
+<div class="col-lg-3 mb-4 col-md-4 col-sm-6">
+  <figure class="figure">
+    <img
+      src="./img/chees.png"
+      class="figure-img img-fluid rounded"
+      alt="..."
+      id="cheese"
+      name="stuffing"
+
+    />
+    <figcaption class="figure-caption text-center">
+      С сыром
+    </figcaption>
+  </figure>
+</div>
+<div class="col-lg-3 mb-4 col-md-4 col-sm-6">
+  <figure class="figure">
+    <img
+      src="./img/salad.png"
+      class="figure-img img-fluid rounded"
+      alt="..."
+      id="salad"
+      name="stuffing"
+    />
+    <figcaption class="figure-caption text-center">
+      С салатом
+    </figcaption>
+  </figure>
+</div>
+
+<div class="col-lg-3 mb-4 col-md-4 col-sm-6">
+  <figure class="figure">
+    <img
+      src="./img/potatoes.png"
+      class="figure-img img-fluid rounded"
+      alt="..."
+      id="potatoes"
+      name="stuffing"
+    />
+    <figcaption class="figure-caption text-center">
+      С картофелем
+    </figcaption>
+  </figure>
+</div>
+
+<div class="col-lg-3 mb-4 col-md-4 col-sm-6">
+  <figure class="figure">
+    <img
+      src="./img/seasoning.png"
+      class="figure-img img-fluid rounded"
+      alt="..."
+      id="seasoning"
+      name="stuffing"
+    />
+    <figcaption class="figure-caption text-center">
+      посыпать приправой
+    </figcaption>
+  </figure>
+</div>
+
+<div class="col-lg-3 mb-4 col-md-4 col-sm-6">
+  <figure class="figure">
+    <img
+      src="./img/mayonnaise.png"
+      class="figure-img img-fluid rounded"
+      alt="..."
+      id="mayonnaise"
+      name="stuffing"
+    />
+    <figcaption class="figure-caption text-center">
+      полить майонезом
+    </figcaption>
+  </figure>
+</div>
+</div>
+`
